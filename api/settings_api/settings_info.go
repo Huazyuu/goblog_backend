9a5d5bbdc13ae43aded43d272fb9827d2a9b8546
zyuu <zyuuforyu@gmail.com>
2024-04-16 14:54:16 +0800

    后端项目搭建

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -2,9 +2,19 @@ package settings_api
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"gvb_server/models/res"
 )
 
 func (SettingsApi *SettingsApi) SettingsInfoView(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"msg": "xxx"})
+	res.Ok(
+		map[string]string{
+			"id":     "123",
+			"name":   "root",
+			"action": "test"},
+		"res.ok", c)
+	//res.OkWithData(map[string]string{"okWithData": "success"}, c)
+	//res.OkWithMessage("res.okWithMessage", c)
+	//res.Fail(map[string]string{}, "res.fail", c)
+	//res.FailWithMessage("res.failWithMag", c)
+	//res.FailWithCode(res.SettingsError, c)
 }
