acb6a2f28166637e68389394e062ef4ec5451cdb
zyuu <zyuuforyu@gmail.com>
2024-05-05 22:44:43 +0800

    user api dev02

--- flag/enter.go
+++ flag/enter.go
@@ -1,23 +1,31 @@
 package flag
 
-import sysFlag "flag"
+import (
+	sysFlag "flag"
+)
 
 type Option struct {
-	DB bool
+	DB   bool
+	User string
 }
 
 // Parse 解析命令行参数
 func Parse() Option {
-	db := sysFlag.Bool("db", false, "初始化数据库")
+	db := sysFlag.Bool("db", false, "初始化数据库 -db")
+	user := sysFlag.String("u", "", "创建用户 -u user")
 	sysFlag.Parse()
 	return Option{
-		DB: *db,
+		DB:   *db,
+		User: *user,
 	}
 }
 
 // IsWebStop 是否停止web项目
 func IsWebStop(option Option) bool {
-	return option.DB
+	if option.DB || option.User == "admin" || option.User == "user" {
+		return true
+	}
+	return false
 }
 
 // SwitchOption 根据命令执行不同函数
@@ -25,4 +33,10 @@ func SwitchOption(option Option) {
 	if option.DB {
 		MakeMigration()
 	}
+	if option.User == "admin" || option.User == "user" {
+		CreateUser(option.User)
+	} else {
+		sysFlag.Usage()
+	}
+
 }
